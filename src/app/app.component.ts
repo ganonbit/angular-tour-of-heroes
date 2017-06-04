5abc1e5de61b35250b6925d4fe6c99abf161ba32
ΔNDRΞW <drewdrummer88@gmail.com>
2017-06-04 06:47:26 -0500

    created service class sharable to many components. used ngOnInit lifecycle hook to get hero data when AppComponent activates. defined HeroService as provider for AppComponent. created mock hero data and imported into the service. designed service to return a Promise and the component to get the data from Promise.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,11 @@
-import { Component } from '@angular/core';
-import { Hero } from './hero';
+import { Component, OnInit } from '@angular/core';
 
-const HEROES: Hero[] = [
-  {id: 11, name: 'Mr. Nice'},
-  {id: 12, name: 'Narco'},
-  {id: 13, name: 'Bombasto'},
-  {id: 14, name: 'Celeritas'},
-  {id: 15, name: 'Magenta'},
-  {id: 16, name: 'RubberMan'},
-  {id: 17, name: 'Dynama'},
-  {id: 18, name: 'Dr IQ'},
-  {id: 19, name: 'Magma'},
-  {id: 20, name: 'Tornado'}
-];
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
 
 @Component({
   selector: 'app-root',
+  providers: [HeroService],
   template: `
   	<h1>{{title}}</h1>
     <h2>My Heroes</h2>
@@ -79,10 +69,20 @@ const HEROES: Hero[] = [
       }
     `]
 })
-export class AppComponent {
-  heroes = HEROES;
+export class AppComponent implements OnInit {
+  heroes: Hero[];
   title = 'Tour of Heroes';
   selectedHero: Hero;
+
+  constructor(private heroService: HeroService) { }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'; //<--ngModel lives here
+
 import { AppComponent } from './app.component';
-import { HeroDetailComponent } from './hero-detail-component';
+import { HeroDetailComponent } from './hero-detail.component';
 
 @NgModule({
   declarations: [
